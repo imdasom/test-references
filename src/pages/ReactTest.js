2fb67f38ad86914ac343274b1801e52b671a9134
imdasom <imdasom101@gmail.com>
2020-01-25 22:37:34 +0900

    src : 불필요한 코드 제거

--- src/pages/ReactTest.js
+++ src/pages/ReactTest.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Header} from 'semantic-ui-react';
 
 import Layout from './Layout';
 import TestExampleList from '../components/TestExampleList.jsx';
@@ -7,7 +6,6 @@ import TestExampleList from '../components/TestExampleList.jsx';
 const ReactTest = () => {
   return (
     <Layout>
-      <Header as="h2">React</Header>
       <TestExampleList />
     </Layout>
   );
